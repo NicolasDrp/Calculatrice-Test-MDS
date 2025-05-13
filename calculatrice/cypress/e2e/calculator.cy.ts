5e5d96b0a3245d9aa978da2eba7e53b56f9b795d
NicolasDrp <drapier.nicolas07@gmail.com>
2025-05-14 00:28:51 +0200

    feat: add additional test cases for calculator functionality including history management

--- calculatrice/cypress/e2e/calculator.cy.ts
+++ calculatrice/cypress/e2e/calculator.cy.ts
@@ -13,6 +13,15 @@ describe("Calculatrice", () => {
     cy.get("div").contains("3");
   });
 
+  it("fait une multiplication simple", () => {
+    cy.contains("3").click();
+    cy.contains("*").click();
+    cy.contains("4").click();
+    cy.contains("=").click();
+
+    cy.get("div").contains("12");
+  });
+
   it("gère une division par 0", () => {
     cy.contains("5").click();
     cy.contains("/").click();
@@ -46,5 +55,57 @@ describe("Calculatrice", () => {
 
     cy.contains("C").click();
     cy.get("div").contains("0");
+  });
+
+  it("ajoute le calcul à l'historique", () => {
+    cy.contains("6").click();
+    cy.contains("-").click();
+    cy.contains("1").click();
+    cy.contains("=").click();
+
+    cy.contains("Historique")
+      .parent()
+      .within(() => {
+        cy.contains("6 - 1 = 5").should("exist");
+      });
+  });
+
+  it("permet de recharger un ancien calcul depuis l'historique", () => {
+    cy.contains("7").click();
+    cy.contains("+").click();
+    cy.contains("2").click();
+    cy.contains("=").click();
+
+    cy.contains("Historique")
+      .parent()
+      .within(() => {
+        cy.contains("7 + 2 = 9").click();
+      });
+
+    cy.get("div").contains("9");
+  });
+
+  it("efface l'historique", () => {
+    cy.contains("8").click();
+    cy.contains("+").click();
+    cy.contains("1").click();
+    cy.contains("=").click();
+
+    cy.contains("Effacer l'historique").click();
+
+    cy.contains("8 + 1 = 9").should("not.exist");
+  });
+
+  it("gère plusieurs opérations consécutives", () => {
+    cy.contains("5").click();
+    cy.contains("+").click();
+    cy.contains("5").click();
+    cy.contains("=").click(); // 10
+
+    cy.contains("*").click();
+    cy.contains("2").click();
+    cy.contains("=").click(); // 20
+
+    cy.get("div").contains("20");
   });
 });
